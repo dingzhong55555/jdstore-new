610b879f7d5b3cacd647b4cf06363bc026f58582
dingzhong <dingzhong55555@gmail.com>
2017-08-08 11:28:05 +0800

    add is_admin to user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_is_admin
+  layout 'admin'
+
+
   def index
     @products = Product.all
   end
@@ -37,6 +42,13 @@ class Admin::ProductsController < ApplicationController
     @product = Product.find(params[:id])
     if @product.destroy
       redirect_to admin_products_path
+    end
+  end
+
+  def require_is_admin
+    if !current_user.admin?
+      redirect_to "/"
+      flash[:warning] = "You are not admin!"
     end
   end
 
